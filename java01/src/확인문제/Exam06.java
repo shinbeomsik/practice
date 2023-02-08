94adaa7ddc29847fcff090da22c357c7574d7492
shinbeomsik <124110586+shinbeomsik@users.noreply.github.com>
2023-02-08 23:23:24 +0900

    2023/02/06에 했던 day06복습

---
+++ java01/src/확인문제/Exam06.java
@@ -0,0 +1,22 @@
+package 확인문제;
+
+public class Exam06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a = 'p';
+		char b = 'q';
+		
+		double data = 2.2;
+		
+		if (a==b && data>2) {
+			System.out.println("오늘은 성공!");
+		} else {
+			System.out.println("내일 다시 도전!");
+		}
+	
+		
+
+	}
+
+}
